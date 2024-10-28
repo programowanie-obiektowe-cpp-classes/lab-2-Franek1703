3d87118e8e2fd00e6e7b6efbcfd8d1c1d509f8de
FRANCISZEK ZAREBSKI <fran.zarebski@gmail.com>
2024-10-28 16:46:58 +0100

    Add first attempt of this task

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,36 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource_;
+
+public:
+    // Konstruktor domyślny
+    ResourceManager() : resource_(new Resource()) {}
+
+    // Konstruktor kopiujący
+    ResourceManager(const ResourceManager& other) : resource_(other.resource_) {}
+
+    // Operator przypisania kopiujący
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            resource_ = other.resource_;
+        }
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) noexcept : resource_(std::move(other.resource_)) {}
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other) {
+            resource_ = std::move(other.resource_);
+        }
+        return *this;
+    }
+
+    double get() const { return resource_->get(); }
+
+    ~ResourceManager() { delete resource_; }
 };
