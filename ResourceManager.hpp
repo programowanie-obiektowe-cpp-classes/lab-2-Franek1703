70559f15274a9eab23842c37488b3c6e2d7f1471
FRANCISZEK ZAREBSKI <fran.zarebski@gmail.com>
2024-12-13 12:55:20 +0100

    Add first attempt for third task

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -23,6 +23,19 @@ public:
         return *this;
     }
 
+    ResourceManager(ResourceManager&& other) noexcept : resource_(other.resource_) {
+        other.resource_ = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this != &other) {
+            delete resource_;
+            resource_ = other.resource_;
+            other.resource_ = nullptr;
+        }
+        return *this;
+    }
+
     double get() const
     {
         if (resource_) {
